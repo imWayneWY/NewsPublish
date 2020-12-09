3cd962cdb4375f8dc00fd819a5683252eacb5939
Wei Yan <im.wayne.wy@gmail.com>
2020-12-09 13:45:00 -0800

    add latest reviewed news

--- NewsPublish.Web/Controllers/NewsController.cs
+++ NewsPublish.Web/Controllers/NewsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NewsPublish.Model.Response;
 using NewsPublish.Service;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -22,17 +23,25 @@ namespace NewsPublish.Web.Controllers
             if (id < 0)
                 Response.Redirect("/Home/Index");
             var classify = _newsService.GetOneNewsClassify(id);
+            ViewData["ClassifyName"] = "News";
+            ViewData["Title"] = "News";
+            ViewData["NewsList"] = new ResponseModel();
+            ViewData["NewCommentNews"] = new ResponseModel();
             if (classify.code == 0)
+            {
                 Response.Redirect("/Home/Index");
-            ViewData["ClassifyName"] = classify.data.Name;
-            ViewData["Title"] = classify.data.Name;
+            } else
+            {
+                ViewData["ClassifyName"] = classify.data.Name;
+                ViewData["Title"] = classify.data.Name;
 
-            var newsList = _newsService.GetNewsList(c => c.NewsClassifyId == id, 6);
-            ViewData["NewsList"] = newsList;
+                var newsList = _newsService.GetNewsList(c => c.NewsClassifyId == id, 6);
+                ViewData["NewsList"] = newsList;
 
-            var newCommentNews = _newsService.GetNewsCommentNewsList(c => c.NewsClassifyId == id, 5);
-            ViewData["NewCommentNews"] = newCommentNews;
+                var newCommentNews = _newsService.GetNewsCommentNewsList(c => c.NewsClassifyId == id, 5);
+                ViewData["NewCommentNews"] = newCommentNews;
 
+            }
             return View(_newsService.GetNewsClassifyList());
         }
     }
